c61434ca8667bc9f164fbe71f7befe37f3535852
GIT_GLOBAL_USER_NAME <GIT_GLOBAL_MAIL>
2020-10-12 15:13:05 +0200

    update asciidoc generation to docker-inside

--- build-html-docker-inside-docker.sh
+++ build-html-docker-inside-docker.sh
@@ -1,18 +1,22 @@
 set -e
 # copy to gh-pages
-BUILD_DIR="docs"
+BUILD_DIR="gh-pages"
 rm -rf -v $BUILD_DIR # else plantuml diagrams won't be rebuilt
 # do not copy revealjs
-mkdir -p docs
+mkdir -p $BUILD_DIR
 cp -r -p -v asciidocs/images $BUILD_DIR/images/
 cp -r -p -v asciidocs/themes $BUILD_DIR
 cp -r -p -v asciidocs/docinfo.html $BUILD_DIR
+cp -r -p -v asciidocs/.nojekyll $BUILD_DIR
+#cp -r -p -v asciidocs/index.adoc $BUILD_DIR
 cp -r -p -v asciidocs/*.adoc $BUILD_DIR
-cp -r -p -v src $BUILD_DIR
+#uncomment it when you want to copy the source code into the gh-pages (for including source code into your document)
+#cp -r -p -v src $BUILD_DIR
 
-docker run --rm \
-           -v ${PWD}/$BUILD_DIR:/documents \
-           asciidoctor/docker-asciidoctor asciidoctor \
+CURRENT_FOLDER=${PWD}
+echo "pwd -> ${CURRENT_FOLDER}"
+echo "adoc-folder->${CURRENT_FOLDER}/${BUILD_DIR}/*.adoc"
+asciidoctor \
            -r asciidoctor-diagram \
            -a icons=font \
            -a experimental=true \
@@ -28,10 +32,10 @@ docker run --rm \
            -a favicon=themes/favicon.png \
            -a sourcedir=src/main/java \
            -b html5 \
-           '*.adoc'
+           "${CURRENT_FOLDER}/${BUILD_DIR}/*.adoc"
+rm -rf ./.asciidoctor
+echo Creating html-docs in Docker finished ...
 
-rm -rf -v $BUILD_DIR/revealjs
-rm -rf -v $BUILD_DIR/.asciidoctor
 rm -rf -v $BUILD_DIR/*.adoc
 rm -v $BUILD_DIR/docinfo.html
 
